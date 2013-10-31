06e0abb0de0a204a8ca7b2cee38a9298742b4363
nrj <nrj@c8fda13e-dad1-a843-9b9c-b67001e4e725>
2013-10-31 18:49:50 +0000

    Helper template methods refactored.
    git-svn-id: svn://localhost/trunk@119 c8fda13e-dad1-a843-9b9c-b67001e4e725

--- helpers.h
+++ helpers.h
@@ -2,43 +2,44 @@
 #define HELPERS_H
 
 #include <QList>
+
 #include "lightfigureposition.h"
 
 class Helpers
 {
 public:
-    template<class T> static void AppendIfNotNull(QList<T>& list, const T& value);
-    template<class T> static void AppendIfNotExists(QList<T>& list, const T& value);
-    template<class T> static void AppendIfNotExists(QList<T>& list, const QList<T>& values);
+    template<typename Container, typename T> static inline void AppendIfNotNull(Container& container, const T& value);
+    template<typename Container, typename T> static inline void AppendIfNotExists(Container& container, const T& value);
+    template<typename T, template<typename T> class Container > static inline void AppendIfNotExists(Container<T>& container, const Container<T>& values);
+
     static void AppendIfValid(PositionList& list, const POSITION& position);
     static void AppendIfValid(PositionList& list, const PositionList& positions);
     static void AppendIfValidAndNotExists(PositionList& list, const POSITION& position);
     static void AppendIfValidAndNotExists(PositionList& list, const PositionList& positions);
 };
 
-template<class T> void Helpers::AppendIfNotNull(QList<T>& list, const T& value)
+template<typename Container, typename T> void Helpers::AppendIfNotNull(Container& container, const T& value)
 {
     if (value != NULL)
     {
-        list.append(value);
+        container.append(value);
     }
 }
 
-template<class T> void Helpers::AppendIfNotExists(QList<T>& list, const T& value)
+template<typename Container, typename T> void Helpers::AppendIfNotExists(Container& container, const T& value)
 {
-    if (!list.contains(value))
+    if (!container.contains(value))
     {
-        list.append(value);
+        container.append(value);
     }
 }
 
-template<class T> void Helpers::AppendIfNotExists(QList<T>& list, const QList<T>& values)
+template<typename T, template<typename T> class Container> void Helpers::AppendIfNotExists(Container<T>& container, const Container<T>& values)
 {
     foreach(T value, values)
     {
-        AppendIfNotExists(list, value);
+        AppendIfNotExists(container, value);
     }
 }
-
 
 #endif // HELPERS_H
